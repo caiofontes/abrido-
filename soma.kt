9a09a36f55e8673923e6ceb2acf3742756195040
caiofontes <caiof0490@gmail.com>
2022-06-14 19:16:08 -0400

    abrido

---
+++ soma.kt
@@ -0,0 +1,16 @@
+fun soma(x:Int,y:Int): Int {
+    return x+y
+}
+fun subtracao(x:Int, y:Int):Int{
+    return x-y
+}
+fun main (){
+    println("soma de dois numeros") 
+    println (soma(4,3))
+    println("subtração de dois numeros")
+    println(subtracao(4,0))
+}
+
+
+    
+
